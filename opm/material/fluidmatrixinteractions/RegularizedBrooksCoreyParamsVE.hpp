9e53d8fb6ced61ad0f85f0fbd7fe0dadbfc29753
Helmer André Friis <hefr@norceresearch.no>
2019-06-06 13:56:24 +0200

    WIP initial VE matriallaw

---
+++ opm/material/fluidmatrixinteractions/RegularizedBrooksCoreyParamsVE.hpp
@@ -0,0 +1,115 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
+*/
+/*!
+ * \file
+ * \copydoc Opm::RegularizedBrooksCoreyParamsVE
+ */
+#ifndef OPM_REGULARIZED_BROOKS_COREY_PARAMS_VE_HPP
+#define OPM_REGULARIZED_BROOKS_COREY_PARAMS_VE_HPP
+
+#include "RegularizedBrooksCorey.hpp"
+#include "RegularizedBrooksCoreyParams.hpp"
+
+#include <cassert>
+
+#include <opm/material/common/EnsureFinalized.hpp>
+
+namespace Opm {
+/*!
+ * \ingroup FluidMatrixInteractions
+ *
+ * \brief   Parameters that are necessary for the \em regularization of
+ *          the Brooks-Corey capillary pressure model.
+ */
+template <class TraitsT>
+class RegularizedBrooksCoreyParamsVE : public Opm::RegularizedBrooksCoreyParams<TraitsT>
+{
+    typedef Opm::RegularizedBrooksCoreyParams<TraitsT> RegularizedBrooksCoreyParams;
+    typedef typename TraitsT::Scalar Scalar;
+
+public:
+    typedef TraitsT Traits;
+
+    RegularizedBrooksCoreyParamsVE()
+        : RegularizedBrooksCoreyParams()
+        , krnEndPoint_(0.01)
+        , krwEndPoint_(0.01)
+    {
+    }
+
+    RegularizedBrooksCoreyParamsVE(Scalar entryPressure, Scalar lambda)
+        : RegularizedBrooksCoreyParams(entryPressure, lambda)
+        , krnEndPoint_(0.01)
+        , krwEndPoint_(0.01)
+    { finalize(); }
+
+    /*!
+     * \brief Calculate all dependent quantities once the independent
+     *        quantities of the parameter object have been set.
+     */
+    void finalize()
+    {
+        RegularizedBrooksCoreyParams::finalize();
+#warning do something
+        krnEndPoint_ = 0.01;
+        krwEndPoint_ = 0.01;
+    }
+
+    /*!
+     * \brief Return the threshold saturation below which the
+     *        capillary pressure is regularized.
+     */
+    Scalar krnEndPoint() const
+    { EnsureFinalized::check(); return krnEndPoint_; }
+
+    /*!
+     * \brief Return the capillary pressure at the low threshold
+     *        saturation of the wetting phase.
+     */
+    Scalar krwEndPoint() const
+    { EnsureFinalized::check(); return krwEndPoint_; }
+
+    /*!
+     * \brief Set the threshold saturation below which the capillary
+     *        pressure is regularized.
+     */
+    void setkrnEndPoint(Scalar value)
+    { krnEndPoint_ = value; }
+
+    /*!
+     * \brief Set the threshold saturation below which the capillary
+     *        pressure is regularized.
+     */
+    void setkrwEndPoint(Scalar value)
+    { krwEndPoint_ = value; }
+
+
+
+private:
+
+    Scalar krnEndPoint_;
+    Scalar krwEndPoint_;
+};
+} // namespace Opm
+
+#endif
---
+++ opm/material/fluidmatrixinteractions/RegularizedBrooksCoreyVE.hpp
@@ -0,0 +1,154 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
+*/
+/*!
+ * \file
+ * \copydoc Opm::RegularizedBrooksCoreyVE
+ */
+#ifndef REGULARIZED_BROOKS_COREY_VE_HPP
+#define REGULARIZED_BROOKS_COREY_VE_HPP
+
+#include "RegularizedBrooksCorey.hpp"
+#include "RegularizedBrooksCoreyParamsVE.hpp"
+
+#include <opm/material/common/Spline.hpp>
+
+namespace Opm {
+/*!
+ * \ingroup FluidMatrixInteractions
+ * \brief Implementation of the regularized Brooks-Corey capillary
+ *        pressure / relative permeability <-> saturation relation.
+ *
+ * This class bundles the "raw" curves as static members and doesn't
+ * concern itself converting absolute to effective saturations and
+ * vice versa.
+ *
+ * In order to avoid very steep gradients the marginal values are
+ * "regularized".  This means that in stead of following the curve of
+ * the material law in these regions, some linear approximation is
+ * used.  Doing this is not worse than following the material
+ * law. E.g. for very low wetting phase values the material laws
+ * predict infinite values for \f$p_c\f$ which is completely
+ * unphysical. In case of very high wetting phase saturations the
+ * difference between regularized and "pure" material law is not big.
+ *
+ * Regularizing has the additional benefit of being numerically
+ * friendly: Newton's method does not like infinite gradients.
+ *
+ * The implementation is accomplished as follows:
+ * - check whether we are in the range of regularization
+ *   - yes: use the regularization
+ *   - no: forward to the standard material law.
+ *
+ * \see BrooksCorey
+ */
+template <class TraitsT, class ParamsT = RegularizedBrooksCoreyParamsVE<TraitsT> >
+class RegularizedBrooksCoreyVE : public TraitsT
+{
+    typedef Opm::RegularizedBrooksCorey<TraitsT, ParamsT> RegularizedBrooksCorey;
+
+public:
+    typedef TraitsT Traits;
+    typedef ParamsT Params;
+    typedef typename Traits::Scalar Scalar;
+
+    //! The number of fluid phases
+    static const int numPhases = Traits::numPhases;
+    static_assert(numPhases == 2,
+                  "The regularized Brooks-Corey capillary pressure law only "
+                  "applies to the case of two fluid phases");
+
+    //! Specify whether this material law implements the two-phase
+    //! convenience API
+    static const bool implementsTwoPhaseApi = true;
+
+    //! Specify whether this material law implements the two-phase
+    //! convenience API which only depends on the phase saturations
+    static const bool implementsTwoPhaseSatApi = true;
+
+    //! Specify whether the quantities defined by this material law
+    //! are saturation dependent
+    static const bool isSaturationDependent = true;
+
+    //! Specify whether the quantities defined by this material law
+    //! are dependent on the absolute pressure
+    static const bool isPressureDependent = false;
+
+    //! Specify whether the quantities defined by this material law
+    //! are temperature dependent
+    static const bool isTemperatureDependent = false;
+
+    //! Specify whether the quantities defined by this material law
+    //! are dependent on the phase composition
+    static const bool isCompositionDependent = false;
+
+    static_assert(Traits::numPhases == 2,
+                  "The number of fluid phases must be two if you want to use "
+                  "this material law!");
+
+    /*!
+     * \brief The capillary pressure-saturation curves depending on absolute saturations.
+     *
+     * \param values A random access container which stores the
+     *               relative pressure of each fluid phase.
+     * \param params The parameter object expressing the coefficients
+     *               required by the material law.
+     * \param fs The fluid state for which the capillary pressure
+     *           ought to be calculated
+     */
+    template <class Container, class FluidState>
+    static void capillaryPressures(Container& values, const Params& params, const FluidState& fs)
+    {
+        RegularizedBrooksCorey::capillaryPressures(values, params, fs);
+    }
+
+    /*!
+     * \brief Calculate the saturations of the phases starting from
+     *        their pressure differences.
+     */
+    template <class Container, class FluidState>
+    static void saturations(Container& values, const Params& params, const FluidState& fs)
+    {
+        RegularizedBrooksCorey::saturations(values, params, fs);
+    }
+
+    /*!
+     * \brief The relative permeability-saturation curves depending on absolute saturations.
+     *
+     * \param values A random access container which stores the
+     *               relative permeability of each fluid phase.
+     * \param params The parameter object expressing the coefficients
+     *               required by the material law.
+     * \param fs The fluid state for which the relative permeabilities
+     *           ought to be calculated
+     */
+    template <class Container, class FluidState>
+    static void relativePermeabilities(Container& values, const Params& params, const FluidState& fs)
+    {
+         RegularizedBrooksCorey::relativePermeabilities(values, params, fs);
+    }
+
+   
+};
+} // namespace Opm
+
+#endif
